0d9f415fca168b798c5b64103b115e26116ab2b8
obaemon <obahajime1291@icloud.com>
2025-12-29 04:35:03 +0900

    pingコマンド動かしてみる

--- main.py
+++ main.py
@@ -1,6 +1,28 @@
-def main():
-    print("Hello from selfishdicebot!")
+import os
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
 
+load_dotenv()
 
-if __name__ == "__main__":
-    main()
+TOKEN = os.getenv("DISCORD_BOT_TOKEN")
+if not TOKEN:
+    raise RuntimeError("DISCORD_BOT_TOKEN is not found.")
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(
+    command_prefix="!",
+    intents=intents,
+)
+
+@bot.event
+async def on_ready():
+    print(f"{bot.user} LOGIN")
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send("Hello?")
+
+bot.run(TOKEN)
